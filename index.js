e0c9e167e4fca60c536d83a85c4d55b7cde05512
Nikita Pikovets <m.pikovets@astoundcommerce.com>
2023-07-24 19:25:06 +0300

    add express server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000; // Change this to the desired port number
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route to render the 'index.html' file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
