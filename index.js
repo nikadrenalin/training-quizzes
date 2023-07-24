ddd421471e908393a1da07ea4b669498aa1953fd
Nikita Pikovets <m.pikovets@astoundcommerce.com>
2023-07-24 19:32:07 +0300

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000; // Change this to the desired port number
+const port = process.env.PORT || 5001;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
